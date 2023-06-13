489b99f4103468496e7c30004e42ca75ea2ba211
HolyRaveOS <kitlaruk1995@gmail.com>
2023-06-13 14:21:31 +0300

    finished product-page

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,7 +4,7 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 // Отримуємо посилання на кнопку "search-form__opnen-btn"
-const openBtn = document.querySelector('.search-form__opnen-btn');
+const openBtn = document.querySelector('.search-form__open-btn');
 
 // Отримуємо посилання на елемент "search-form__box"
 const searchBox = document.querySelector('.search-form__box');
@@ -41,6 +41,35 @@ chooseItems.forEach(item => {
 		checkbox.setAttribute('aria-checked', '');
 	});
 });
+
+
+if (document.body.classList.contains('product-page-script')) {
+	// Функція, яка буде виконуватись при зміні розміру вікна
+	function checkWindowSize() {
+		// Отримуємо ширину екрану
+		var windowWidth = window.innerWidth;
+
+		// Отримуємо елемент, на якому потрібно змінити значення атрибуту
+		var element = document.getElementById("showmore-list"); // Замініть "your-element-id" на фактичний ідентифікатор вашого елемента
+
+		// Перевіряємо ширину екрану та змінюємо значення атрибуту "data-showmore-content"
+		if (windowWidth <= 904 && windowWidth > 479) {
+			element.setAttribute("data-showmore-content", "3");
+		}
+		else if (windowWidth <= 479)
+			element.setAttribute("data-showmore-content", "4");
+
+		else if (windowWidth > 904) {
+			element.setAttribute("data-showmore-content", "2");
+		}
+	}
+
+	// Додаємо обробник події зміни розміру вікна
+	window.addEventListener("resize", checkWindowSize);
+
+	// Викликаємо функцію при завантаженні сторінки (якщо потрібно перевірити ширину екрану відразу)
+	checkWindowSize();
+}
 
 
 
